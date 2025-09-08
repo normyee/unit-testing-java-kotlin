38229cc414736d037434796069012cb2cd964385
normyee <lunoqro@gmail.com>
2025-09-08 13:45:43 -0300

    feat: add recipe entity

--- unitjava/src/main/java/com/normyee/unitjava/core/RecipeEntity.java
+++ unitjava/src/main/java/com/normyee/unitjava/core/RecipeEntity.java
@@ -1,4 +1,30 @@
 package com.normyee.unitjava.core;
 
-public class recipeEntity {
+public class RecipeEntity {
+    private Integer _id;
+    private String _name;
+    private String[] _ingredients;
+    public Integer getId() {
+        return _id;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public String[] getIngredients() {
+        return _ingredients;
+    }
+
+    public void setId(Integer id) {
+        this._id = id;
+    }
+
+    public void setName(String name) {
+        this._name = name;
+    }
+
+    public void setIngredients(String[] ingredients) {
+        this._ingredients = ingredients;
+    }
 }
