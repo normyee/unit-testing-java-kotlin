29c20049ed51d116675f1f11e602c5d7bef79a1a
normyee <lunoqro@gmail.com>
2025-09-08 13:46:15 -0300

    feat: add recipe service

--- unitjava/src/main/java/com/normyee/unitjava/core/RecipeService.java
+++ unitjava/src/main/java/com/normyee/unitjava/core/RecipeService.java
@@ -1,4 +1,12 @@
 package com.normyee.unitjava.core;
 
-public class recipeService {
+public class RecipeService {
+    private final IRecipeRepository _recipeRepository;
+    public RecipeService(IRecipeRepository recipeRepository ) {
+        this._recipeRepository = recipeRepository;
+    }
+
+//    public RecipeDTO create() {
+//
+//    }
 }
