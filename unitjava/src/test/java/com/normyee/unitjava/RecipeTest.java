5cd24e79cc1d0a4a25dca0e09a741a886b90578b
normyee <lunoqro@gmail.com>
2025-09-08 13:22:43 -0300

    test: add failing test

---
+++ unitjava/src/test/java/com/normyee/unitjava/RecipeTest.java
@@ -0,0 +1,14 @@
+package com.normyee.unitjava;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RecipeTest {
+   private final RecipeService recipeService = new RecipeService();
+
+    @Test
+    @DisplayName("it should create a new recipe")
+    public void createRecipeTest() {
+
+    }
+}
