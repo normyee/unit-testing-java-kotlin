a5baca458c4cb4d95c2165d8537e57c5789edff4
normyee <lunoqro@gmail.com>
2025-09-08 13:47:43 -0300

    test: improve test

--- unitjava/src/test/java/com/normyee/unitjava/RecipeTest.java
+++ unitjava/src/test/java/com/normyee/unitjava/RecipeTest.java
@@ -1,14 +1,17 @@
 package com.normyee.unitjava;
 
+import com.normyee.unitjava.core.RecipeService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class RecipeTest {
-   private final RecipeService recipeService = new RecipeService();
+   private final RecipeService _recipeService = new RecipeService();
 
     @Test
-    @DisplayName("it should create a new recipe")
+    @DisplayName("it should create a new recipe and not be null")
     public void createRecipeTest() {
-
+        var recipe = this._recipeService.create();
+        Assertions.assertNotNull(recipe);
     }
 }
