8b84d310b5c9b541cd4cc20965c722f74e79cf4f
normyee <lunoqro@gmail.com>
2025-09-08 22:55:40 -0300

    test: add fake class

--- kotlin/src/test/kotlin/fake/MainFake.kt
+++ kotlin/src/test/kotlin/fake/MainFake.kt
@@ -1,4 +1,26 @@
 package fake
 
+import org.example.IPetRepository
+import org.example.Pet
+
 class MainFake {
+
+    class PetRepositoryFake : IPetRepository {
+        private var _storage = mutableListOf<Pet>()
+
+        override fun create(pet: Pet): Pet {
+
+            if(_storage.size < 1) pet.id = 1 else pet.id = _storage.size
+
+            _storage.add(pet)
+
+            return _storage.last()
+        }
+
+        override fun findById(id: Int): Pet? {
+            return _storage.firstOrNull { it.id == id }
+        }
+    }
+
+
 }
